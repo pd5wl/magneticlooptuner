dc2e52c1c5874c7904f44b6ea4e250c397f88201
Wijnand Laros <pd5wl@dkars.nl>
2016-11-09 19:21:19 +0100

    Add files via upload
    Added the CAT support for Yeasu

--- magneticlooptuner.ino
+++ magneticlooptuner.ino
@@ -2,19 +2,19 @@
 #include "Stepper.h"
 #include <Wire.h>  // Comes with Arduino IDE
 #include <LiquidCrystal_I2C.h>
-/*#include <SoftwareSerial.h>
-#include <FT857D.h>*/
+#include <SoftwareSerial.h>
+#include "FT857D.h"     // the file FT857D.h has a lot of documentation which I've added to make using the library easier
+
 
 /*-----( Declare Constants )-----*/
-#define STEPS  32   // Number of steps for one revolution of Internal shaft
-                    // 2048 steps for one revolution of External shaft
-#define BACKLIGHT_PIN 13
+#define STEPS  32   // Number of steps for one revolution of Internal shaft 2048 steps for one revolution of External shaft
+#define BACKLIGHT_PIN 13 //Enable Backlight Pin
 
 /*-----( Declare objects )-----*/
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
-Stepper small_stepper(STEPS, 8, 10, 9, 11); // Setup of proper sequencing for Motor Driver Pins
-                                            // In1, In2, In3, In4 in the sequence 1-3-2-4
-                                            
+Stepper small_stepper(STEPS, 8, 10, 9, 11); // Setup of proper sequencing for Motor Driver Pins In1, In2, In3, In4 in the sequence 1-3-2-4
+FT857D radio;           // define "radio" so that we may pass CAT commands
+
 /*-----( Declare Variables )-----*/ 
 volatile boolean TurnDetected;  // need volatile for Interrupts
 volatile boolean rotationdirection;  // CW or CCW rotation
@@ -39,19 +39,27 @@ void isr ()  {
 }
  
 void setup ()  {
-// LCD
-  lcd.begin(16,2);   // initialize the lcd for 16 chars 2 lines, turn on backlight
+// Setup Radio
+  SoftwareSerial userSerial(6,7);  
+  radio.setSerial(userSerial);
+  Serial.begin(38400);
+  radio.begin(38400);    // as with Serial.begin(9600); we wish to start the software serial port
+                        // so that we may control a radio via CAT commands
+  // LCD 16x2 and backlight
+  lcd.begin(16,2);   
+  pinMode ( BACKLIGHT_PIN, OUTPUT );   
+  digitalWrite ( BACKLIGHT_PIN, HIGH ); 
   lcd.clear();
-  lcd.setCursor(0,0); //Start at character 4 on line 0
+  lcd.setCursor(0,0); 
   lcd.print("MagLoopTunner");
   lcd.setCursor(0,1);
   lcd.print("PD5WL/Ver. 0.1b");
-   // Switch on the backlight
-  pinMode ( BACKLIGHT_PIN, OUTPUT );
-  digitalWrite ( BACKLIGHT_PIN, HIGH ); 
   delay(3000);
   lcd.clear();
+  lcd.setCursor(0,0);
   lcd.print("Rotorposition");
+  lcd.setCursor(10,0);
+  lcd.print("Freq");
 // Rotery  
   pinMode(PinCLK,INPUT);
   pinMode(PinDT,INPUT);  
@@ -61,7 +69,6 @@ void setup ()  {
 }
  
 void loop ()  {
-
   small_stepper.setSpeed(600); //Max seems to be 700
   if (!(digitalRead(PinSW))) {   // check if button is pressed
     if (RotaryPosition == 0) {  // check if button was already pressed
@@ -92,8 +99,11 @@ void loop ()  {
       small_stepper.step(StepsToTake);
     }
   }
-  lcd.setCursor(3,1);
+  lcd.setCursor(0,1);
   lcd.print(RotaryPosition);  
+  lcd.setCursor(5,1);         
+  lcd.print(radio.getFreqMode());
+
 }
 
 
